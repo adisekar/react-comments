7b9d96438cc17f9c161bd055c02f323e20f89f34
Adi <adithya.sekar@ibm.com>
2020-03-28 11:13:11 -0400

    React Approval card and comments checkin

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
+
+const Comment = () => {
+  return (
+    <div className="ui comments">
+      <h3 className="ui dividing header">Cards</h3>
+      <ApprovalCard>
+        <CommentDetail avatar={faker.image.avatar()} author={faker.name.firstName()} content={faker.lorem.sentence()} />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail avatar={faker.image.avatar()} author={faker.name.firstName()} content="This has been very useful for my research. Thanks as well!" />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail avatar={faker.image.avatar()} author={faker.name.firstName()} content="Dude, this is awesome. Thanks so much" />
+      </ApprovalCard>
+    </div>
+  );
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Comment />,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
